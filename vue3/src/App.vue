b31f58c3b03d6823ced33abcec9fd09cff27d7b0
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2022-01-01 14:46:45 +0100

    test vue

---
+++ vue3/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import formulaireTest from './components/formulaireTest.vue'
+
+</script>
+
+<template>
+<article>
+  <HelloWorld msg="vue Project" />
+  <formulaireTest/>
+</article>
+</template>
+
+<style>
+body{
+  margin: 0;
+}
+*{
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ vue3/src/components/formulaireTest.vue
@@ -0,0 +1,37 @@
+<template>
+<div ref='element' class="formulaire">
+    <p v-if="nom.length>3">{{nom}}</p>
+    <input type="text" v-model="nom">
+    <button v-on:click="submitt" type="submit">valide ma gueule </button>
+</div>
+</template>
+
+
+<script setup>
+import { ref,onMounted } from 'vue';
+
+let nom =ref('')
+let element=ref()
+function submitt(){
+    console.log(`nom=${nom.value}`)
+    /*console.log('nom:'+nom.value)*/
+}
+onMounted(()=>{
+element.value.classList='active'
+})
+</script>
+
+
+<style>
+.formulaire{
+    height: 20vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+}
+.active{
+    background-color: aquamarine;
+}
+</style>
